59345d06d3889fcb5720d3815cb59cbd4070c680
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-09-08 16:45:27 +0100

    Rspec Joke job

--- lib/joke/rspec.rb
+++ lib/joke/rspec.rb
@@ -1,7 +1,13 @@
 module Joke
   module Rspec
+      
       def self.run
         system('RUBY -S rspec')
       end
+
+      def self.to_proc
+        Proc.new { self.run }
+      end
+
   end
 end
